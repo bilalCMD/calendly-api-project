6bb296542435da5d1eaaa0f9f690d471c4511d9e
bilalCMD <113253401+bilalCMD@users.noreply.github.com>
2025-07-31 18:37:33 +0500

    Update availability.js

--- api/availability.js
+++ api/availability.js
@@ -1,17 +1,22 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const { start_time, end_time, timezone, event_type } = req.query;
+  try {
+    const { start_time, end_time, timezone, event_type } = req.query;
 
-  const response = await fetch(
-    `https://api.calendly.com/availability?event_type=${event_type}&start_time=${start_time}&end_time=${end_time}&timezone=${timezone}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.CALENDLY_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  const data = await response.json();
-  res.status(200).json(data);
+    const response = await fetch(
+      `https://api.calendly.com/availability?event_type=${event_type}&start_time=${start_time}&end_time=${end_time}&timezone=${timezone}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.CALENDLY_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
