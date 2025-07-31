6618478d61d16aca649f2ef76c3f81f969a29d92
bilalCMD <113253401+bilalCMD@users.noreply.github.com>
2025-07-31 19:13:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 module.exports = (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.end(`
-    <html>
-      <head><title>Calendly API</title></head>
-      <body>
-        <h1>Calendly API Working!</h1>
-        <p>Use endpoints: <code>/api/availability</code> or <code>/api/create-link</code></p>
-      </body>
-    </html>
-  `);
+  res.status(200).send("Calendly API is live 🚀");
 };
